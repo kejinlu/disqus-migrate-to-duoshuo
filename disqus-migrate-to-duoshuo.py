069c5862f9a218635335a78ea1f39e4ae5146f73
kejinlu <kejinlu@gmail.com>
2016-08-03 00:10:50 +0800

    增加逻辑

--- disqus-migrate-to-duoshuo.py
+++ disqus-migrate-to-duoshuo.py
@@ -1,7 +1,25 @@
 from xml.etree.ElementTree import parse
+import json
+from urllib.parse import urlparse
 
 xmlFile = open('./data.xml')
 dom = parse(xmlFile)
-dom.getroot().getchildren()
+thread_elements = dom.getroot().findall('{http://disqus.com}thread')
+#threads
+threads = []
+for thread_element in thread_elements:
+    thread = {}
+    title = thread_element.find('{http://disqus.com}title').text
+    link = thread_element.find('{http://disqus.com}link').text
+    if link.startswith('http://geeklu.com') and title:
+        thread_key = urlparse(link).path
+        thread['thread_key'] = thread_key
+        if title.endswith(' | 卢克'):
+            title = title[:-5]
+        thread['title'] = title
+        thread['url'] = link
+        threads.append(thread)
 
 
+#posts
+print(threads)
