a8274b2ab28d892bd1f7b0ef7a7e4c974101a22a
kejinlu <kejinlu@gmail.com>
2016-08-03 09:19:14 +0800

    update

--- disqus-migrate-to-duoshuo.py
+++ disqus-migrate-to-duoshuo.py
@@ -2,11 +2,13 @@ from xml.etree.ElementTree import parse
 import json
 from urllib.parse import urlparse
 
+title_suffix = ' | 卢克'
 xmlFile = open('./data.xml')
 dom = parse(xmlFile)
 thread_elements = dom.getroot().findall('{http://disqus.com}thread')
 #threads
 threads = []
+threads_map = {} #便于关联构建输出时进行索引
 for thread_element in thread_elements:
     thread = {}
     title = thread_element.find('{http://disqus.com}title').text
@@ -14,12 +16,22 @@ for thread_element in thread_elements:
     if link.startswith('http://geeklu.com') and title:
         thread_key = urlparse(link).path
         thread['thread_key'] = thread_key
-        if title.endswith(' | 卢克'):
-            title = title[:-5]
+        if title.endswith(title_suffix):
+            title = title[:-len(title_suffix)]
         thread['title'] = title
         thread['url'] = link
         threads.append(thread)
 
+        thread_id = thread_element.get('{http://disqus.com/disqus-internals}id')
+        threads_map[thread_id] = thread
 
 #posts
-print(threads)
+post_elements = dom.getroot().findall('{http://disqus.com}post')
+posts = []
+for post_element in post_elements:
+    post = {}
+    email = post_element.find('{http://disqus.com}author/{http://disqus.com}email').text
+    print(email)
+
+
+#print(threads)
